623d9aa2f6532669c41502dba9201db60477a134
Snehal <snehal2004@gmail.com>
2025-02-09 15:45:37 +0530

    more debugs 3.3

--- app/components/Connect.tsx
+++ app/components/Connect.tsx
@@ -8,7 +8,7 @@ const Connect = () => {
     <div className='bg-gradient-to-b from-[#264073] via-[#264073] via-29% to-[#c600ff] to-82% h-auto min-h-screen w-full' id="Connect">
         <WaveDivider clr="#6162b8" />
         <h1 className='text-white text-[250%] px-6 py-2 font-extrabold text-center'>Connect</h1>
-        <div className='w-full h-[80%] flex flex-wrap flex-col justify-center items-center gap-11 pt-8'>
+        <div className='w-full h-[80%] flex flex-wrap flex-col justify-center items-center gap-11 pt-8 pb-4 px-2'>
             <Mail/>
             <div>
                 <Socials /> 
